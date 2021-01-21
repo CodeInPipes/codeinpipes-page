60e96bba54abc40dafebe3914126b85e463052f4
CodeInPipes <codeinpipes@outlook.com>
2021-01-21 10:42:04 -0300

    add ProfilePage

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Profile = () => (
+   <div className="Profile-wrapper">
+    <h1>CodeInPipes</h1>
+    <h2>DevOps and Lifelong Student</h2>
+    <p>Criador de conteúdo, escrevo sobre o mundo da automação em DEV, QA e PROD</p>
+  </div>
+)
+
+export default Profile
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from 'gatsby'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,16 +6,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>CodeInPipes</h1>
-    <ul>
-      <li>
-        <Link to="/" activeStyle={{ color: 'red'}}>Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-    </ul>
-    
+    <h1>CodeInPipes</h1>       
   </Layout>
 )
 
